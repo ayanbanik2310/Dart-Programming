4498b757e389c62d20de95ed5fd877e6c99b3b25
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-02-21 02:29:21 +0600

    datatypes.dart

---
+++ datatypes.dart
@@ -0,0 +1,52 @@
+// data types 
+
+void main()
+{
+  int n=10;
+  print(n);
+  double a=10.5;
+  print(a);
+  String s="Ayan Banik";
+  print(s);
+  bool flag=true;
+  print(flag);
+
+  int hexa=0xA;
+  print(hexa);
+
+  String easy1='it\'s easy';
+  String easy2="it's easy";
+  print(easy1);
+  print(easy2);
+
+  String bigs="Hi i am ayan banik."
+              "I read in class 5";
+  print(bigs);
+
+  String name="ayan";
+  String message1="my name is " + name;
+  String message2="my name is $name";
+  print(message1);
+  print(message2);
+  print("my name is $name");
+  print("the name of characters string $name is ${name.length}" );
+  print("the name of characters string $name is " + name.length.toString());
+  
+  int x=10, y=20;
+  int sum=x+y;
+  print("sum of $x and $y = ${sum}");
+}
+
+/*
+variables - 
+
+int
+double
+String
+bool
+list (known as array)
+maps
+runes
+symbols
+
+*/
