7329127d96abcff79353eb5f5a6c262542f58e63
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-02-22 02:17:56 +0600

    Create const_final_keyword.dart

---
+++ const_final_keyword.dart
@@ -0,0 +1,23 @@
+void main()
+{
+
+  // final keyword
+  final cityname="sylhet";
+  // cityname="habiganj";  // it will show error if we want to change the value
+  print(cityname);
+
+  final String countryName="bangladesh";
+  print(countryName);
+
+  // const keyword
+  const PI= 3.14;
+  const gravity=9.8;
+  print(PI);
+  print(gravity);
+
+}
+/*
+
+final and const used for those values which we don't want to change  
+
+*/
