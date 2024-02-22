3dd3742f5cc5403d4bb558a0f21befbdfc39bcbc
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-02-22 16:27:17 +0600

    Create if_else.dart

---
+++ if_else.dart
@@ -0,0 +1,34 @@
+void main(){
+  var age=10;
+  if(age<5)
+  {
+    print("child");
+  }
+  else if(age<12)
+  {
+    print("boy");
+  }
+  else if(age<21)
+  {
+    print("adult");
+  }
+  else
+  {
+    print("men");
+  } 
+
+  var mark=90;
+  if(mark<100 && mark>80) print("very good");
+  else if(mark>50 && mark<80) print("good");
+  else print("thik hai");
+
+  int a=2;
+  int b=3;
+  if(a+b==5){
+    print("ayan");
+  }
+  else{
+    print("banik");
+  }
+
+}
