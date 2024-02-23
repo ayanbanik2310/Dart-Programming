269d504a7e92f828e60f0bc4623ccc030954d03f
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-02-24 00:42:43 +0600

    Create switch_case.dart

---
+++ switch_case.dart
@@ -0,0 +1,22 @@
+void main() {
+  var grade = 'A';
+  switch (grade) {
+    case 'A':
+      print("you are good");
+      break;
+    case 'B':
+      print("you are ok");
+      break;
+    case 'C':
+      print("you are bad");
+      break;
+    case 'F':
+      print("you are fail");
+      break;
+    default:
+      print("invalid");
+  }
+}
+/*
+switch case statements only applicable for int string variable
+*/
