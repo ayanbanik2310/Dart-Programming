33d069250316b78eb29cd9e56177f9c23a5a1d4e
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-02-24 00:33:52 +0600

    Create ternary.dart

---
+++ ternary.dart
@@ -0,0 +1,9 @@
+void main() {
+  var a = 10, b = 20;
+  (a < b) ? print("b=$b is greater") : print("b=$b is greater");
+
+  var smallnumber = (a < b) ? a : b;
+  print("smallnumber is $smallnumber");
+
+
+}
