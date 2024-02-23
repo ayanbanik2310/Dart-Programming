6535631df06c9517ad5317f3e9c8af3842956e94
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-02-24 01:19:44 +0600

    Update input.dart

--- input.dart
+++ input.dart
@@ -1,21 +1,24 @@
 import 'dart:io';
 
 void main() {
+  //String input
   print("enter your name ");
   String name = stdin.readLineSync()!;
   print("your name is $name");
 
+  //integer input
   print("enter your age ");
   // var age = stdin.readLineSync(); // age is string , to convert this into int follow next line
   int age = int.parse(stdin.readLineSync()!);
   print("age is $age");
 
+  //double input
   print("enter your cgpa ");
   double cg = double.parse(stdin.readLineSync()!);
   print("your cgpa is $cg");
 
 
-  // variable check 
+  // datatype check
   
   if (age is int) {
     print("$age is integer");
